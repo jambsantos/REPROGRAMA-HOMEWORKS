b7d57a255bdf41fb54273e890755f7b4a56d7e76
jessica-MBS <jessica.aparecida@gmail.com>
2020-08-30 09:01:38 -0300

    exercicios de for e semana3 feitos e testados

--- 3SEMANA/exerciciosfor.js
+++ 3SEMANA/exerciciosfor.js
@@ -70,6 +70,117 @@ function imparPar(num) {
 }
 console.log(imparPar(4))
 
+// 6) crie uma função que receba um número inteiro e imprima os números no console de 1 ao número. 
+// Porém, para todos os números que forem múltiplos de 3, imprima na tela "banana" ao invés do número; 
+// para os números múltiplos de 5, imprima "biscoito". Para números múltiplos de 3 e 5, imprima "biscoito de banana". 
+// Exemplo, recebendo 15 o resultado será 1 2 banana 4 biscoito banana 7 8 banana biscoito 11 banana 13 14 biscoito de banana
+
+function crazySeq(num){
+    let i = 1 
+    while (i<=num) {
+        if (i%3 ===0 && i%5 !=0) {
+            console.log(`banana`)}
+        else if (i%3 != 0 && i%5 ===0){
+            console.log(`biscoito`)}
+        else if (i%3 ===0 && i%5 ===0){
+            console.log(`biscoito de banana`)}
+        else { console.log(i)}
+        i++
+    }
+}
+
+// **** Outa forma de resolver questao 6 com for em vez de while ****
+crazySeq(15)
+
+function malucaSeq(num){
+    for(let i=1; i<=num; i++){
+        if (i%3 ===0 && i%5 ===0){
+            console.log(`biscoito de banana`)}
+        else if (i%3 ===0 && i%5 !=0){
+            console.log(`banana`)}
+        else if (i%3 !=0 && i%5 ===0){
+            console.log(`biscoito`)}
+        else {console.log(i)}
+    }
+}
+
+malucaSeq(30)
+
+// 7) crie uma função que receba um número positivo inteiro, itere de 1 ao número e some todos os números múltiplos 
+// de 3 OU 5.
+
+function somaTresCinco (num){
+    let soma = 0 
+    for(let i=0; i<=num; i++){
+        if (i%3===0 || i%5===0){
+            soma +=i
+        }
+    }
+    console.log(soma)
+}
+
+somaTresCinco(5)
+
+// 8) altere a função acima para, ao invés de somar múltiplos de 5 ou 3, fazer isso com qualquer número de 1 a 9.
+// Os números serão recebidos via parâmetro de função, por exemplo: `function somaMultiplos(num, multiplo1, multiplo2)` 
+// pode ser chamada com os valores `somaMultiplos(100, 2, 4) e verificar os números múltiplos de 2 e 4. 
+// Extra: antes de seguir com o cálculo, verificar se os números recebidos estão mesmo entre 1 e 9; 
+// scaso não estejam, imprimir o aviso "informe múltiplos de 1 a 9" e encerrar o programa.
+
+function somaMulti(num, mult1,mult2) {
+    if (mult1<1 && mult2>9) {
+        console.log("informe múltiplos de 1 a 9")}
+let soma =0 
+for(let i=0; i<=num; i++){
+    if (i% mult1 ===0 || i% mult2 ===0){
+    soma +=i}
+}
+  console.log(soma)  
+}
+
+somaMulti(100,2,4)          // somaMulti(4,2,4) **teste com números q consigo o resultado de cabeça
+                     
+// 9) crie uma função que receba um número qualquer devolva o seguinte padrão como resultado (inclusive com os espaços). Por exemplo, caso receba o número 5 como parâmetro:
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+
+function arvore(num){
+    let arv = ''
+    for(let i=1; i<=num; i++) {
+        arv +=`*`
+        console.log(arv)
+    }
+}
+
+arvore(5)
+
+///////// ESSE DESAFIO EXTRA NÃO ESTÁ FUNCIONANDO CORRETAMENTE ///////////
+
+// DESAFIO EXTRA: refaça o exercício acima, porém agora com o seguinte padrão:
+//     *
+//    **
+//   ***
+//  ****
+// *****
+
+function arvoreInv(num){
+    let arv = ''
+    let vaz = ''
+    for(let ln=0; ln<=num; ln++) {
+        for(let cl= num-ln; cl>1; cl--){
+            vaz +=' '
+        }
+        arv +=`*`
+        console.log(arv + vaz)
+    }
+}
+
+arvoreInv(5)
+
 
 
 
